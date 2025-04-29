792e7df2f44282cf102b3c1efd062cb089fd80a7
Phuc Thai <bindirt@gmail.com>
2025-04-29 19:14:50 +0700

    A sample of chat with tools using local Ollama (#2)

---
+++ GenAI-Samples/EverythingServer/Program.cs
@@ -0,0 +1,230 @@
+﻿using EverythingServer;
+using EverythingServer.Prompts;
+using EverythingServer.Tools;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol;
+using ModelContextProtocol.Protocol.Types;
+using ModelContextProtocol.Server;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+HashSet<string> subscriptions = [];
+var _minimumLoggingLevel = LoggingLevel.Debug;
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools<AddTool>()
+    .WithTools<AnnotatedMessageTool>()
+    .WithTools<EchoTool>()
+    .WithTools<LongRunningTool>()
+    .WithTools<PrintEnvTool>()
+    .WithTools<SampleLlmTool>()
+    .WithTools<TinyImageTool>()
+    .WithPrompts<ComplexPromptType>()
+    .WithPrompts<SimplePromptType>()
+    .WithListResourcesHandler(async (ctx, ct) =>
+    {
+        return new ListResourcesResult
+        {
+            Resources =
+            [
+                new ModelContextProtocol.Protocol.Types.Resource { Name = "Direct Text Resource", Description = "A direct text resource", MimeType = "text/plain", Uri = "test://direct/text/resource" },
+            ]
+        };
+    })
+    .WithListResourceTemplatesHandler(async (ctx, ct) =>
+    {
+        return new ListResourceTemplatesResult
+        {
+            ResourceTemplates =
+            [
+                new ResourceTemplate { Name = "Template Resource", Description = "A template resource with a numeric ID", UriTemplate = "test://template/resource/{id}" }
+            ]
+        };
+    })
+    .WithReadResourceHandler(async (ctx, ct) =>
+    {
+        var uri = ctx.Params?.Uri;
+
+        if (uri == "test://direct/text/resource")
+        {
+            return new ReadResourceResult
+            {
+                Contents = [new TextResourceContents
+                {
+                    Text = "This is a direct resource",
+                    MimeType = "text/plain",
+                    Uri = uri,
+                }]
+            };
+        }
+
+        if (uri is null || !uri.StartsWith("test://template/resource/"))
+        {
+            throw new NotSupportedException($"Unknown resource: {uri}");
+        }
+
+        int index = int.Parse(uri["test://template/resource/".Length..]) - 1;
+
+        if (index < 0 || index >= ResourceGenerator.Resources.Count)
+        {
+            throw new NotSupportedException($"Unknown resource: {uri}");
+        }
+
+        var resource = ResourceGenerator.Resources[index];
+
+        if (resource.MimeType == "text/plain")
+        {
+            return new ReadResourceResult
+            {
+                Contents = [new TextResourceContents
+                {
+                    Text = resource.Description!,
+                    MimeType = resource.MimeType,
+                    Uri = resource.Uri,
+                }]
+            };
+        }
+        else
+        {
+            return new ReadResourceResult
+            {
+                Contents = [new BlobResourceContents
+                {
+                    Blob = resource.Description!,
+                    MimeType = resource.MimeType,
+                    Uri = resource.Uri,
+                }]
+            };
+        }
+    })
+    .WithSubscribeToResourcesHandler(async (ctx, ct) =>
+    {
+        var uri = ctx.Params?.Uri;
+
+        if (uri is not null)
+        {
+            subscriptions.Add(uri);
+
+            await ctx.Server.RequestSamplingAsync([
+                new ChatMessage(ChatRole.System, "You are a helpful test server"),
+                new ChatMessage(ChatRole.User, $"Resource {uri}, context: A new subscription was started"),
+            ],
+            options: new ChatOptions
+            {
+                MaxOutputTokens = 100,
+                Temperature = 0.7f,
+            },
+            cancellationToken: ct);
+        }
+
+        return new EmptyResult();
+    })
+    .WithUnsubscribeFromResourcesHandler(async (ctx, ct) =>
+    {
+        var uri = ctx.Params?.Uri;
+        if (uri is not null)
+        {
+            subscriptions.Remove(uri);
+        }
+        return new EmptyResult();
+    })
+    .WithCompleteHandler(async (ctx, ct) =>
+    {
+        var exampleCompletions = new Dictionary<string, IEnumerable<string>>
+        {
+            { "style", ["casual", "formal", "technical", "friendly"] },
+            { "temperature", ["0", "0.5", "0.7", "1.0"] },
+            { "resourceId", ["1", "2", "3", "4", "5"] }
+        };
+
+        if (ctx.Params is not { } @params)
+        {
+            throw new NotSupportedException($"Params are required.");
+        }
+
+        var @ref = @params.Ref;
+        var argument = @params.Argument;
+
+        if (@ref.Type == "ref/resource")
+        {
+            var resourceId = @ref.Uri?.Split("/").Last();
+
+            if (resourceId is null)
+            {
+                return new CompleteResult();
+            }
+
+            var values = exampleCompletions["resourceId"].Where(id => id.StartsWith(argument.Value));
+
+            return new CompleteResult
+            {
+                Completion = new Completion { Values = [.. values], HasMore = false, Total = values.Count() }
+            };
+        }
+
+        if (@ref.Type == "ref/prompt")
+        {
+            if (!exampleCompletions.TryGetValue(argument.Name, out IEnumerable<string>? value))
+            {
+                throw new NotSupportedException($"Unknown argument name: {argument.Name}");
+            }
+
+            var values = value.Where(value => value.StartsWith(argument.Value));
+            return new CompleteResult
+            {
+                Completion = new Completion { Values = [.. values], HasMore = false, Total = values.Count() }
+            };
+        }
+
+        throw new NotSupportedException($"Unknown reference type: {@ref.Type}");
+    })
+    .WithSetLoggingLevelHandler(async (ctx, ct) =>
+    {
+        if (ctx.Params?.Level is null)
+        {
+            throw new McpException("Missing required argument 'level'", McpErrorCode.InvalidParams);
+        }
+
+        _minimumLoggingLevel = ctx.Params.Level;
+
+        await ctx.Server.SendNotificationAsync("notifications/message", new
+        {
+            Level = "debug",
+            Logger = "test-server",
+            Data = $"Logging level set to {_minimumLoggingLevel}",
+        }, cancellationToken: ct);
+
+        return new EmptyResult();
+    });
+
+ResourceBuilder resource = ResourceBuilder.CreateDefault().AddService("everything-server");
+builder.Services.AddOpenTelemetry()
+    .WithTracing(b => b.AddSource("*").AddHttpClientInstrumentation().SetResourceBuilder(resource))
+    .WithMetrics(b => b.AddMeter("*").AddHttpClientInstrumentation().SetResourceBuilder(resource))
+    .WithLogging(b => b.SetResourceBuilder(resource))
+    .UseOtlpExporter();
+
+builder.Services.AddSingleton(subscriptions);
+builder.Services.AddHostedService<SubscriptionMessageSender>();
+builder.Services.AddHostedService<LoggingUpdateMessageSender>();
+
+builder.Services.AddSingleton<Func<LoggingLevel>>(_ => () => _minimumLoggingLevel);
+
+await builder.Build().RunAsync();
